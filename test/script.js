3059b9695f14b44626e2da7dc56d0acdd47ff8b1
Stanislas Marion <stanislas.marion@gmail.com>
2014-12-21 16:56:07 +0100

    fix getAddress for p2sh

--- test/script.js
+++ test/script.js
@@ -50,4 +50,35 @@ describe('Script', function() {
     var decoded = bcoin.script.decode(encoded);
     assert(bcoin.script.isNullData(decoded))
   })
+
+  describe('Address', function () {
+    it('should get an address from a p2pkh output script', function () {
+      var hex = 'a9143e71b020e16a160f2fe9e17421800317ceddb8db87'
+      var encoded = utils.toArray(hex, 'hex')
+      assert.equal(bcoin.script.getAddress(encoded, 'testnet3'), '2MxwQ5MjWJTqecb2nnawMH9eX6cjnb3HgND')
+    })
+
+    it('should get an address from a p2pkh input script', function () {
+      var hex = '493046022100bb3c194a30e460d81d34be0a230179c043a656f67e3c5c8bf47eceae7c4042ee0221008bf54ca11b2985285be0fd7a212873d243e6e73f5fad57e8eb14c4f39728b8c6014104e365859b3c78a8b7c202412b949ebca58e147dba297be29eee53cd3e1d300a6419bc780cc9aec0dc94ed194e91c8f6433f1b781ee00eac0ead2aae1e8e0712c6'
+      var encoded = utils.toArray(hex, 'hex')
+      var decoded = bcoin.script.decode(encoded)
+      assert(bcoin.script.isPubkeyhashIn(decoded))
+      assert.equal(bcoin.script.getAddress(decoded, 'mainnet'), '127k9DmeZa7JAwxG2TPHTTMbnwP181vNYb');
+      //assert.equal(bcoin.script.getAddress(encoded, 'testnet3'), '2MxwQ5MjWJTqecb2nnawMH9eX6cjnb3HgND')
+    })
+
+    // TODO
+    //it('should get an address from a p2pk output script', function () {
+      //var hex = 'a9143e71b020e16a160f2fe9e17421800317ceddb8db87'
+      //var encoded = utils.toArray(hex, 'hex')
+      //assert.equal(bcoin.script.getAddress(encoded, 'testnet3'), '2MxwQ5MjWJTqecb2nnawMH9eX6cjnb3HgND')
+    //})
+
+    it('should get an address from a p2sh output script', function () {
+      var hex = 'a9143e71b020e16a160f2fe9e17421800317ceddb8db87'
+      var encoded = utils.toArray(hex, 'hex')
+      assert.equal(bcoin.script.getAddress(encoded, 'testnet3'), '2MxwQ5MjWJTqecb2nnawMH9eX6cjnb3HgND')
+    })
+  })
+
 });
